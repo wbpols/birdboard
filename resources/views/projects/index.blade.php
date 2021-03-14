1731eb56265099cd7f5d62c2dc565002a69fcf65
wbpols <w.b.pols@gmail.com>
2021-03-14 15:49:33 +0100

    Commit episode 9 "Go Go Gadget Tailwind"

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -5,13 +5,16 @@
         <a href="{{ route('projects.create') }}">Create New Project</a>
     </div>
 
-    <ul>
+    <div class="flex">
         @forelse ($projects as $project)
-            <li>
-                <a href="{{ $project->path() }}">{{ $project->title }}</a>
-            </li>
+            <div class="bg-white mr-4 rounded p-5 shadow w-1/3" style="height: 200px;">
+                {{-- <a href="{{ $project->path() }}"> --}}
+                    <h3 class="font-normal text-xl mb-4 py-4">{{ $project->title }}</h3>
+                {{-- </a> --}}
+                <div class="text-gray-500">{{ \Illuminate\Support\Str::limit($project->description, 100) }}</div>
+            </div>
         @empty
-            <li>No Projects yet!</li>
+            <div>No Projects yet!</div>
         @endforelse
-    </ul>
+    </div>
 @endsection
